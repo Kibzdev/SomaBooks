dbb47b0a4c9813d369fa6cd18fbd9c0fde5aa628
Kibzdev <fkibuthu20@gmail.com>
2025-12-04 21:27:46 +0300

    admin create book feature added

--- database/schema.ts
+++ database/schema.ts
@@ -8,7 +8,7 @@ export  const BORROW_STATUS_ENUM = pgEnum('borrow_status', [ 'RETURNED', 'BORROW
 
 
 
-export const users = pgTable('users', {
+export const users = pgTable("users", {
    id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
   fullName: varchar("full_name", { length:255 }).notNull(),
   email: text("email").notNull().unique(),
@@ -21,5 +21,20 @@ export const users = pgTable('users', {
   createdAt: timestamp('created_at',{
     withTimezone: true,
   }).defaultNow(),
+});
 
+export const books = pgTable("books", {
+  id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+  title: varchar("title", { length: 255 }).notNull(),
+  author: varchar("author", { length: 255 }).notNull(),
+  genre: text("genre").notNull(),
+  rating: integer("rating").notNull(),
+  coverUrl: text("cover_url").notNull(),
+  coverColor: varchar("cover_color", { length: 7 }).notNull(),
+  description: text("description").notNull(),
+  totalCopies: integer("total_copies").notNull().default(1),
+  availableCopies: integer("available_copies").notNull().default(0),
+  videoUrl: text("video_url").notNull(),
+  summary: varchar("summary").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
 });
