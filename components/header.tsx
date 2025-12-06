4a206f1a66448808d5eddd2a7937dbdbfe5ccb26
Kibzdev <fkibuthu20@gmail.com>
2025-12-06 13:42:20 +0300

    header fix

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 import Link from "next/link";
-import { signOut } from '../auth';
-import { Button } from './ui/button';
+import Image from "next/image";
+import { signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
-
   return (
     <header className="my-10 flex justify-between gap-5">
       <Link href="/">
@@ -13,25 +12,20 @@ const Header = () => {
 
       <ul className="flex flex-row items-center gap-8">
         <li>
-          <form action={async () => {
-                      "use server";
-                      await signOut();
-               }}
-               className="mb-10"
-               
-               >
-                <Button>Logout</Button>
+          <form
+            action={async () => {
+              "use server";
+
+              await signOut();
+            }}
+            className="mb-10"
+          >
+            <Button>Logout</Button>
           </form>
-          {/* <Link href="/my-profile">
-            <Avatar>
-              <AvatarFallback className="bg-amber-100">
-                {getInitials(session?.user?.name || "IN")}
-              </AvatarFallback>
-            </Avatar>
-          </Link> */}
         </li>
       </ul>
     </header>
   );
 };
+
 export default Header;
