db1fcc3b529218dfa99058ef8ca23949d9e97707
Kibzdev <fkibuthu20@gmail.com>
2025-12-06 13:32:30 +0300

    fix if check for borrow btn

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,15 +1,15 @@
-import Image from 'next/image'
-import BookCover from './BookCover'
-import BorrowBook from './BorrowBook'
-import { db } from '../database/drizzle';
-import { users } from '../database/schema';
-import { eq } from 'drizzle-orm';
+import React from "react";
+import Image from "next/image";
+import BookCover from "@/components/BookCover";
+import BorrowBook from "@/components/BorrowBook";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
-interface Props extends Book{
+interface Props extends Book {
   userId: string;
 }
-
-const BookOverview = async ({ 
+const BookOverview = async ({
   title,
   author,
   genre,
@@ -26,60 +26,77 @@ const BookOverview = async ({
     .select()
     .from(users)
     .where(eq(users.id, userId))
-    .limit(1)
+    .limit(1);
 
-    if(!user) return null;
-
- const borrowingEligibility = {
-  isEligible: availableCopies> 0 && user.status === 'APPROVED',
-  message: 
-    availableCopies <=0
-    ? "Book is not available" : "You are eligible to borrow this book",
- }
-
+  const borrowingEligibility = {
+    isEligible: availableCopies > 0 && user?.status === "APPROVED",
+    message:
+      availableCopies <= 0
+        ? "Book is not available"
+        : "You are not eligible to borrow this book",
+  };
   return (
-   <section className="book-overview">
-     <div className="flex flex-1 flex-col gap-5">
-      <h1>{title}</h1>
-      <div className="book-info">
-        <p> 
-          By <span className="font-semibold text-light-200">{author}</span>
-        </p>
-         <p> 
-          Category: <span className="font-semibold text-light-200">{genre}</span>
-        </p>
-        <div className="flex flex-row-gap-1">
-          <Image src="/icons/star.svg" alt="star icon" width={22} height={22} />
-           <p>{rating}</p>   
+    <section className="book-overview">
+      <div className="flex flex-1 flex-col gap-5">
+        <h1>{title}</h1>
+
+        <div className="book-info">
+          <p>
+            By <span className="font-semibold text-light-200">{author}</span>
+          </p>
+
+          <p>
+            Category{" "}
+            <span className="font-semibold text-light-200">{genre}</span>
+          </p>
+
+          <div className="flex flex-row gap-1">
+            <Image src="/icons/star.svg" alt="star" width={22} height={22} />
+            <p>{rating}</p>
+          </div>
+        </div>
+
+        <div className="book-copies">
+          <p>
+            Total Books <span>{totalCopies}</span>
+          </p>
+
+          <p>
+            Available Books <span>{availableCopies}</span>
+          </p>
+        </div>
+
+        <p className="book-description">{description}</p>
+
+        {user && (
+          <BorrowBook
+            bookId={id}
+            userId={userId}
+            borrowingEligibility={borrowingEligibility}
+          />
+        )}
+      </div>
+
+      <div className="relative flex flex-1 justify-center">
+        <div className="relative">
+          <BookCover
+            variant="wide"
+            className="z-10"
+            coverColor={coverColor}
+            coverImage={coverUrl}
+          />
+
+          <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
+            <BookCover
+              variant="wide"
+              coverColor={coverColor}
+              coverImage={coverUrl}
+            />
+          </div>
         </div>
       </div>
-      <div className='book-copie'>
-        <p>
-          Total Books: <span>{totalCopies}</span>
-        </p>
-        <p>
-          Available Books: <span>{availableCopies}</span>
-        </p>
-      </div>
-      <p className="book-description">{description}</p>
+    </section>
+  );
+};
 
-      <BorrowBook bookId={id} userId={userId} borrowingEligibility={borrowingEligibility}/>
-     </div>
-
-     <div className="relative flex flex-1 justify-center">
-      <div className='relative'>
-        <BookCover
-          variant="wide"
-          className="z-10"
-          coverColor={coverColor}
-          coverImage={coverUrl} 
-        />
-        <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
-          <BookCover variant="wide" coverColor={coverColor} coverImage={coverUrl} />
-        </div>
-      </div>
-     </div>
-   </section>
-  )
-}
-export default BookOverview
+export default BookOverview;
